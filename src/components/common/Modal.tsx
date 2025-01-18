9aca2ffe72dc45cfa4ab3c43f24e6c771420a3a8
JungYunHan <hanjy2008@naver.com>
2025-01-19 03:14:57 +0900

    feat: 모달 추가

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import { ReactNode } from 'react'
+
+export const Modal = ({ children }: { children: ReactNode }) => {
+  return (
+    <Overlay>
+      <Container>{children}</Container>
+    </Overlay>
+  )
+}
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 100;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 318px;
+  padding: 20px;
+  border-radius: 20px;
+  background-color: #fff;
+  z-index: 1001;
+`
