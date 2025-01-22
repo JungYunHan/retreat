1d4c9822d873e48816fe8b1799e2df206ab9366b
JungYunHan <hanjy2008@naver.com>
2025-01-23 04:27:29 +0900

    style: 세로 스크롤 가능하도록 수정

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -4,13 +4,12 @@ import styled from 'styled-components'
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   width: 100%;
-  height: 100%;
   margin-inline: auto;
   max-width: 430px;
   min-width: 320px;
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: auto;
 `
 
 const DefaultLayout = () => {
