bf99ca84a2b739abdbd973e39f2120190c432f11
JungYunHan <hanjy2008@naver.com>
2025-01-23 04:29:49 +0900

    style: 영상 퀴즈 페이지 디자인 수정

--- src/pages/VideoQuiz/VideoQuestionOnePage/index.styled.ts
+++ src/pages/VideoQuiz/VideoQuestionOnePage/index.styled.ts
@@ -3,28 +3,31 @@ import styled from 'styled-components'
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   width: 100%;
   max-width: 430px;
   min-width: 320px;
-  height: 100%;
   gap: 40px;
-  padding: 20px 0;
+  padding-top: 50px;
 `
 
 export const Title = styled.div`
   font-family: GmarketSans, sans-serif;
   font-size: 20px;
-  font-weight: bold;
+  font-weight: 400;
   line-height: 1.5;
   text-align: center;
+`
+
+export const Strong = styled.strong`
+  font-weight: 600;
 `
 
 export const Input = styled.input`
   font-family: Pretendard, sans-serif;
   width: 250px;
   height: 30px;
+  text-align: center;
   font-size: 16px;
   font-weight: 600;
   border: 2px solid black;
